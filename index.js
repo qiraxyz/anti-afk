eddd509875e7fbd603e6726c3463f5deb6502f3e
Muhamad Ariq Azis A <ariqaziz74@gmail.com>
2023-01-17 20:59:56 +0700

    update latest data #1

---
+++ index.js
@@ -0,0 +1,21 @@
+// global variable
+var auto; 
+var play_afk;
+var setTime;
+var DOM;
+
+// set time click 15 min = 900000ms
+setTime = 900000;
+
+// set id canvas in game
+DOM = 'canvas';
+
+// get canvas games
+auto = document.getElementById(DOM);
+
+function play() {
+    play_afk = setInterval(function(){
+        auto.click()
+    }, setTime);
+    play_afk.start()   
+}
