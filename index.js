1febd9dae8df9b8ab9a720ec4603a593518b378e
Muhamad Ariq Azis A <ariqaziz74@gmail.com>
2023-01-22 12:56:23 +0700

    encrypt data

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-// global variable
-var auto; 
-var play_afk;
-var setTime;
-var DOM;
-
-// set time click 15 min = 900000ms
-setTime = 1000;
-
-// set id canvas in game
-DOM = 'canvas';
-
-// get canvas games
-auto = document.getElementById(DOM);
-
-    setInterval(function(){
-        auto.click(console.log('auto afk on'))
-    }, setTime);
+var _0x408113=_0x13a4;function _0x13a4(_0x3f56fb,_0x47a8f3){var _0x1d47c5=_0x1d47();return _0x13a4=function(_0x13a44f,_0x5ab639){_0x13a44f=_0x13a44f-0x13e;var _0x43fd65=_0x1d47c5[_0x13a44f];return _0x43fd65;},_0x13a4(_0x3f56fb,_0x47a8f3);}function _0x1d47(){var _0x5bc617=['679229DIYhIf','getElementById','canvas','3742895ZtzCHt','2629624UDBhwO','464SrtnJk','5147322axqqOz','2188122zURDJo','log','5605404GJsfbW','auto\x20afk\x20on','3uAKZci'];_0x1d47=function(){return _0x5bc617;};return _0x1d47();}(function(_0x4f6cc8,_0xe62971){var _0x4fb695=_0x13a4,_0x3e2229=_0x4f6cc8();while(!![]){try{var _0x3dcf87=parseInt(_0x4fb695(0x13f))/0x1+parseInt(_0x4fb695(0x146))/0x2*(parseInt(_0x4fb695(0x13e))/0x3)+-parseInt(_0x4fb695(0x144))/0x4+-parseInt(_0x4fb695(0x142))/0x5+-parseInt(_0x4fb695(0x145))/0x6+parseInt(_0x4fb695(0x148))/0x7+-parseInt(_0x4fb695(0x143))/0x8;if(_0x3dcf87===_0xe62971)break;else _0x3e2229['push'](_0x3e2229['shift']());}catch(_0x407dd9){_0x3e2229['push'](_0x3e2229['shift']());}}}(_0x1d47,0x9bf39));var auto,play_afk,setTime,DOM;setTime=0x3e8,DOM=_0x408113(0x141),auto=document[_0x408113(0x140)](DOM),setInterval(function(){var _0x376ccb=_0x408113;auto['click'](console[_0x376ccb(0x147)](_0x376ccb(0x149)));},setTime);
